75db2c66373e0cdfe67d53699294e9fd19a1fadc
doketrushant <trushant.doke@gmail.com>
2018-10-29 01:08:20 +0530

    add new calculations

--- personal-finance-viewer/src/bank-savings/components/bank-savings-pie-chart/InputFormatter.js
+++ personal-finance-viewer/src/bank-savings/components/bank-savings-pie-chart/InputFormatter.js
@@ -12,3 +12,14 @@ export const getTotalAmountByBankByAccountType = (bankSavings, accountType) => b
   value: Number(getSumofAccounts(bankSaving.accounts.filter(account => account.type === accountType))),
   color: bankSaving.color
 }))
+
+const getFixedDepositsAccounts = (bankSaving) => bankSaving.accounts.filter(acc => acc.type = "FIXED");
+const getMaturedCompundedAmount = (yearlyRate, years, startingAmount) =>
+  (Number(startingAmount) * Math.pow((1 + (Number(yearlyRate) / (1 * 100))), (1 * Number(years))));
+
+export const getFDMaturedAmounts = bankSavings => bankSavings.map(bankSaving => ({
+  name: bankSaving.bankName,
+  value: getFixedDepositsAccounts(bankSaving).reduce((accm, account) =>
+    accm = Number(accm) + getMaturedCompundedAmount(account.interestRate, account.years, account.amount), 0),
+  color: bankSaving.color
+}))  
