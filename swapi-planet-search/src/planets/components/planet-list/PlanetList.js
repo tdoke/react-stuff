32ac560562006700c815cc2536f4d17fa1af155b
doketrushant <trushant.doke@gmail.com>
2018-11-08 22:29:53 +0530

    align planet list

--- swapi-planet-search/src/planets/components/planet-list/PlanetList.js
+++ swapi-planet-search/src/planets/components/planet-list/PlanetList.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import FlexView from 'react-flexview/lib';
+import './PlanetList.css'
 
 const PlanetList = ({ planets }) => (
-  <div>
-    {
-      planets.map((planet, index) => 
-        <div key={index} style={{ fontSize: (100+index*20)+'%' }}>
-          <span>{planet.name}</span><span>({planet.population})</span>
-        </div>
-      )
-    }
+  <div className="planet-list-container">
+    <FlexView column>
+      {
+        planets.map((planet, index) =>
+          <FlexView className="planet-row" key={index} width={500} marginBottom={16}>
+            <FlexView hAlignContent="left" width="50%">{planet.name}</FlexView>
+            <FlexView hAlignContent="right" width="50%">{planet.population}</FlexView>
+          </FlexView>
+        )
+      }
+    </FlexView>
   </div>
 )
 export default PlanetList
--- swapi-planet-search/src/planets/components/search-planet-bar/SearchPlanetBar.js
+++ swapi-planet-search/src/planets/components/search-planet-bar/SearchPlanetBar.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import FlexView from 'react-flexview/lib';
 import TextField from '@material-ui/core/TextField'
 import './SearchPlanetBar.css'
 
 const SearchPlanetBar = ({ onSearchInputChange }) => (
   <div className="search-planet-bar-container">
+  <FlexView width={500}>
     <TextField
       label="Planet"
       fullWidth
       onChange={(event) => onSearchInputChange(event.target.value)}
     />
+    </FlexView>
   </div>
 )
 
