e0ba01cd7947c0321c6f7ba00be9878c2dce2bde
doketrushant <trushant.doke@gmail.com>
2018-11-09 23:20:08 +0530

    add debounce

--- swapi-planet-search/src/planets/components/search-planet-bar/SearchPlanetBar.js
+++ swapi-planet-search/src/planets/components/search-planet-bar/SearchPlanetBar.js
@@ -7,17 +7,23 @@ import './SearchPlanetBar.css'
 export default class SearchPlanetBar extends React.PureComponent {
   constructor(props) {
     super(props)
+    this.debouncedOnSearchInputChange = debounce(this.debouncedOnSearchInputChange, 500)
   }
 
+  debouncedOnSearchInputChange = (value) => {
+    value.length && this.props.onSearchInputChange(value)
+  }
+
+  handleSearchInputChange = (event) => this.debouncedOnSearchInputChange(event.target.value)
+
   render() {
-    const { onSearchInputChange } = this.props;
     return (
       <div className="search-planet-bar-container">
         <FlexView width={500}>
           <TextField
             label="Planet"
             fullWidth
-            onChange={(event) => onSearchInputChange(event.target.value)}
+            onChange={this.handleSearchInputChange}
           />
         </FlexView>
       </div>
