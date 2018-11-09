e6c2330693e6befaa49ff993461d1f5f7f3661c5
doketrushant <trushant.doke@gmail.com>
2018-11-09 21:36:30 +0530

    change to class

--- swapi-planet-search/src/planets/components/search-planet-bar/SearchPlanetBar.js
+++ swapi-planet-search/src/planets/components/search-planet-bar/SearchPlanetBar.js
@@ -1,18 +1,26 @@
 import React from 'react'
-import FlexView from 'react-flexview/lib';
+import debounce from 'lodash/debounce'
+import FlexView from 'react-flexview/lib'
 import TextField from '@material-ui/core/TextField'
 import './SearchPlanetBar.css'
 
-const SearchPlanetBar = ({ onSearchInputChange }) => (
-  <div className="search-planet-bar-container">
-  <FlexView width={500}>
-    <TextField
-      label="Planet"
-      fullWidth
-      onChange={(event) => onSearchInputChange(event.target.value)}
-    />
-    </FlexView>
-  </div>
-)
+export default class SearchPlanetBar extends React.PureComponent {
+  constructor(props) {
+    super(props)
+  }
 
-export default SearchPlanetBar
+  render() {
+    const { onSearchInputChange } = this.props;
+    return (
+      <div className="search-planet-bar-container">
+        <FlexView width={500}>
+          <TextField
+            label="Planet"
+            fullWidth
+            onChange={(event) => onSearchInputChange(event.target.value)}
+          />
+        </FlexView>
+      </div>
+    )
+  }
+}
