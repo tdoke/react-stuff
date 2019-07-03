0c61d247ccb205f40366ee2a48bec0c9a00d70e1
Trushant Doke <trushant.doke@xebia.com>
2019-07-03 13:16:00 +0530

    add unit test

--- unit-testing-react/src/components/__tests__/CommentBox.test.js
+++ unit-testing-react/src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,14 @@ it('should have textarea and submit button', () => {
   expect(wrapper.find('textarea').length).toEqual(1)
   expect(wrapper.find('button').length).toEqual(1)
 })
+
+it('has a text area that a user can type in', () => {
+  wrapper.find('textarea').simulate('change', {
+    target: {
+      value: 'test'
+    }
+  })
+
+  wrapper.update()
+  expect(wrapper.find('textarea').prop('value')).toEqual('test')
+})
