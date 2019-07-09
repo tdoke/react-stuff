9af791d8f9efbfb3ba79e4b1e773f315ccd4091e
Trushant Doke <trushant.doke@xebia.com>
2019-07-09 17:31:02 +0530

    add helmet

--- react-setups/react-personal-boilerplate/src/client/routes/home/home.js
+++ react-setups/react-personal-boilerplate/src/client/routes/home/home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
 
 class Home extends Component {
   constructor(props) {
@@ -7,6 +8,7 @@ class Home extends Component {
   }
   render() {
     return (<div>
+      <Helmet><title>Home</title></Helmet>
       <h2>This is home component</h2>
     </div>);
   }
